d655c2e58f2f0adc17a608d938e813092662c960
Kosay Hatem <kosay.hatem@gmail.com>
2026-01-06 06:22:56 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7211011860-c8b46",
+  "appId": "1:535914222664:web:d49fd32f1b3c28499e747d",
+  "apiKey": "AIzaSyAS8P1exzn-PoFIhp96bGxyijiEMf5Jdaw",
+  "authDomain": "studio-7211011860-c8b46.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "535914222664"
+};
